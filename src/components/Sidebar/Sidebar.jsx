7c51e732f71b5800884fac7dbece0b775397b182
Luigi Arena <luigiarena.cs@gmail.com>
2026-04-01 18:07:47 +0200

    Create Sidebar component for category news selection

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../ThemeContext'
+import './Sidebar.css'
+
+function Sidebar() {
+    const { darkMode } = useContext(ThemeContext);
+
+    const categories = [
+        'business',
+        'entertainment',
+        'general',
+        'health',
+        'science',
+        'sports',
+        'technology',
+        'world'
+    ];
+
+    return (
+        <div className={"sidebar" + (darkMode ? " dark" : "")}>
+            <h2>Categories</h2>
+            <ul>
+                {categories.map((category) => (
+                    <li className="category" key={category}>{category}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
