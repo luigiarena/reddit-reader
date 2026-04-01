1e8f614103cfc7e11474bf1ce3e87ac633523747
Luigi Arena <luigiarena.cs@gmail.com>
2026-04-01 19:31:27 +0200

    Integrate newsApi in Card component and Card List

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,35 +1,57 @@
 import "./Card.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../ThemeContext";
+import newsApi from "../../features/api/newsApi";
 
-function Card({ title }) {
+function Card({ cardInfo }) {
     const { darkMode } = useContext(ThemeContext);
+    const { title, author, content, description, publishedAt, urlToImage, url, source} = cardInfo;
+    const sourceName = cardInfo.source.name;
+    const formattedDate = new Date(publishedAt).toLocaleDateString();
 
     return (
         <div className={(darkMode ? "dark" : "") + " card"}>
-            <h2>{title}</h2>
-            <p>This is a simple card component.</p>
+            <div className="card-header">
+                <h2>{title}</h2>
+            </div>
+            <div className="card-body">
+                <img src={urlToImage} alt="" />
+                <p>{description}</p>
+            </div>   
+            <div className="card-footer">
+                <span>Source: {sourceName}</span>
+                <span>Date: {formattedDate}</span>
+            </div>
         </div>
     )
 }
 
 function CardsContainer() {
-  return (
-    <div className="cards-container">
-        <Card title="Card 1" />
-        <Card title="Card 2" />
-        <Card title="Card 3" />
-                <Card title="Card 3" />
-                <Card title="Card 3" />
-                <Card title="Card 3" />
-                <Card title="Card 3" />
-                <Card title="Card 3" />
-                <Card title="Card 3" />
-                <Card title="Card 3" />
-                <Card title="Card 3" />
-                <Card title="Card 3" />
-    </div>
-  )
+    const [newsList, setNewsList] = useState([]);
+
+    useEffect(() => {
+        async function fetchNews() {
+            const data = await newsApi.getNews();
+            setNewsList(data);
+        }
+        fetchNews();
+    }, []);
+
+    return (
+        <div className="cards-container">
+            {
+                !newsList ?
+
+                () => {
+                    return (<><Card /><Card /></>)
+                } :
+
+                newsList.slice().map((item, index) => {
+                    return <Card key={index} cardInfo={item} />
+                })
+            }
+        </div>
+    )
 }
 
 export default CardsContainer;
