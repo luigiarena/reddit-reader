b06529ebcf5dbbab0bad8b50a0b80745cd9217c7
Luigi Arena <luigiarena.cs@gmail.com>
2026-04-01 19:36:31 +0200

    Add All selection for news Categories

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -5,11 +5,19 @@ import { ThemeContext } from "../ThemeContext";
 export default function SearchBar () {
     const { darkMode } = useContext(ThemeContext);
 
+    function handleSearch(event) {
+        console.log(darkMode);
+    }
+
     return (
         <div className={(darkMode ? "dark" : "") + " search-box"}>
             <input type="text" placeholder="Search" 
                 className="" />
             <button className="search-btn"><i className="fa fa-search"></i></button>
+            <button className="search-btn"
+                onClick={handleSearch}>
+                <i className="fa fa-search"></i>
+            </button>
         </div>
     )
 }
