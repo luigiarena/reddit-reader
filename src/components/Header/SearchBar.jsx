fbaab44b92da72cb542d8e3887fa1a7f687bd3e8
Luigi Arena <luigiarena.cs@gmail.com>
2026-04-06 20:43:14 +0200

    Add search functionality with the use of setQuery in searchbar

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,22 +1,31 @@
 import "./Header.css";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { ThemeContext } from "../ThemeContext";
+import { useDispatch, useSelector } from "react-redux";
+import { setQuery } from "../../features/slices/searchSlice";
 
 export default function SearchBar () {
     const { darkMode } = useContext(ThemeContext);
+    //const [searchTerm, setSearchTerm] = useState('');
+    const dispatch = useDispatch();
+    const searchTerm = useSelector(
+        (state) => state.search.query
+    );
 
     function handleSearch(event) {
-        console.log(darkMode);
+        event.preventDefault();
+        dispatch(setQuery(event.target.value));
     }
 
     return (
         <div className={(darkMode ? "dark" : "") + " search-box"}>
             <input type="text" placeholder="Search" 
-                className="" />
-            <button className="search-btn"
-                onClick={handleSearch}>
+                className="" value={searchTerm} onChange={handleSearch}/>
+            <Link className="search-btn"
+                to={`/search/${searchTerm}`}>
                 <i className="fa fa-search"></i>
-            </button>
+            </Link>
         </div>
     )
 }
